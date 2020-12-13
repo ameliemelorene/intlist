2f8d82e37470bdbf391c3271c1fce0cf34b8ae00
ameliemelorene <72452572+ameliemelorene@users.noreply.github.com>
2020-12-13 14:45:00 +0100

    Create intlist.h

---
+++ intlist.h
@@ -0,0 +1,34 @@
+#include<iostream>
+
+class IntList {
+    
+    
+public:
+    void add_front(){
+    }
+    void add_back(){
+    }
+    void print(){
+    }
+    void remove_front(){
+    }
+    void remove_back(){
+    }
+    void remove(int valeur){
+    }
+    bool search(int valeur){
+    }
+
+private:
+
+
+    ~
+
+
+
+};
+
+class IntCell {
+    friend class IntList
+};
+
