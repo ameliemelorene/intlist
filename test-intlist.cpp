6775bac194476fdf042485612607b735274d73c8
ameliemelorene <72452572+ameliemelorene@users.noreply.github.com>
2020-12-15 17:48:12 +0100

    Update test-intlist.cpp

--- test-intlist.cpp
+++ test-intlist.cpp
@@ -1,2 +1,12 @@
-#include<iostream>
-#include "intlist.h"
+#include "test-intlist2.h"
+
+int main(){
+    test_default_constructor_1();
+    //test_add_front_1 ();
+    //test_add_front_2 ();
+    //test_add_back_1 ();
+    //test_add_back_2 ();
+    //test_size_1 ();
+    //test_size_2 ();  
+    return 0;
+}
