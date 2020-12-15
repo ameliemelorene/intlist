39976834ad31d08ebc4e9171460b70c1d5836caf
ameliemelorene <72452572+ameliemelorene@users.noreply.github.com>
2020-12-15 17:49:54 +0100

    Update intlist.h
    base de intcell et créateur de liste

--- intlist.h
+++ intlist.h
@@ -1,34 +1,37 @@
 #include<iostream>
+using namespace std;
 
 class IntList {
-    
-    
 public:
-    void add_front(){
+    IntList (): adrTop(nullptr),adrBot(nullptr),taille(0){}
+    //~IntList();
+    bool is_empty(){
+        return taille==0;
     }
-    void add_back(){
+    int size(){
+        return taille;
     }
-    void print(){
-    }
-    void remove_front(){
-    }
-    void remove_back(){
-    }
-    void remove(int valeur){
-    }
-    bool search(int valeur){
-    }
+    int get_element_at (int place);
+    void add_back(int valeur);
+    void add_front();
+    void print();
+    void remove_front();
+    void remove_back();
+    void remove(int valeur);
+    bool search(int valeur);
 
 private:
-
-
-    ~
-
-
-
+    int taille;
+    int* adrTop;
+    int* adrBot;
 };
 
 class IntCell {
-    friend class IntList
+    friend class IntList;
+private :
+    IntCell(int value,int* av,int* ap):valeur(value),adrB(av),adrA(ap) {
+    }
+    int valeur;
+    int* adrB;
+    int* adrA; 
 };
-
