8e0eafafe94a2614d35101f839bf00a72595182d
ameliemelorene <72452572+ameliemelorene@users.noreply.github.com>
2020-12-22 23:31:55 +0100

    Update intlist.h
    ajout de add_front et add_back

--- intlist.h
+++ intlist.h
@@ -1,9 +1,18 @@
 #include<iostream>
 using namespace std;
 
+class IntCell {
+    friend class IntList;
+private :
+    IntCell(): Before(nullptr),After(nullptr),valeur(0){}
+    int valeur;
+    IntCell * Before;
+    IntCell * After;
+};
+
 class IntList {
 public:
-    IntList (): adrTop(nullptr),adrBot(nullptr),taille(0){}
+    IntList (): Top(nullptr),Bot(nullptr),taille(0){}
     //~IntList();
     bool is_empty(){
         return taille==0;
@@ -12,26 +21,51 @@ public:
         return taille;
     }
     int get_element_at (int place);
-    void add_back(int valeur);
-    void add_front();
+    void add_back(int value){
+        if (is_empty()){
+            IntCell* cell = new IntCell;
+            (*cell).valeur=value;
+            Top=cell;
+            Bot=cell;
+            taille=taille+1;
+        }
+        else{
+            IntCell* cell=new IntCell;
+            (*cell).valeur=value;
+            IntCell* Pointeur_precedent=Bot;
+            (*Pointeur_precedent).After=cell;
+            (*cell).Before=Pointeur_precedent;
+            Bot=cell;
+            taille=taille+1;
+        }
+    }
+    void add_front(int value){
+        if (is_empty()){
+            IntCell* cell = new IntCell;
+            (*cell).valeur=value;
+            Top=cell;
+            Bot=cell;
+            taille=taille+1;
+        }
+        else{
+            IntCell* cell=new IntCell;
+            (*cell).valeur=value;
+            IntCell* Pointeur_suivant=Top;
+            (*Pointeur_suivant).Before=cell;
+            (*cell).After=Pointeur_suivant;
+            Top=cell;
+            taille=taille+1;
+        }
+    }
+
     void print();
     void remove_front();
     void remove_back();
-    void remove(int valeur);
-    bool search(int valeur);
+    void remove(int value);
+    bool search(int value);
 
 private:
     int taille;
-    int* adrTop;
-    int* adrBot;
-};
-
-class IntCell {
-    friend class IntList;
-private :
-    IntCell(int value,int* av,int* ap):valeur(value),adrB(av),adrA(ap) {
-    }
-    int valeur;
-    int* adrB;
-    int* adrA; 
+    IntCell* Top;
+    IntCell* Bot;
 };
