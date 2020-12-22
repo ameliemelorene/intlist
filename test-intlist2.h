bea2a58566fcff9f5e9ec8cf9da1411c07c7bc11
ameliemelorene <72452572+ameliemelorene@users.noreply.github.com>
2020-12-23 00:10:44 +0100

    Update test-intlist2.h
    ajout de nouveaux tests

--- test-intlist2.h
+++ test-intlist2.h
@@ -7,3 +7,25 @@ inline void test_default_constructor_1 () {
   IntList list;
   std::cout << std::boolalpha <<  " *** " << ((list.size() == 0) and (list.is_empty())) << std::endl;
 }
+void test_print() {
+  IntList list;
+  int e1=1;
+  int e2=2;
+  int e3=3;
+  list.add_front(e2);
+  list.add_front(e1);
+  list.add_back(e3);
+  list.print();
+}
+
+inline void test_search(){
+  std::cout << "test search one element"<<std::endl;
+  IntList list;
+  int e1=1;
+  int e2=2;
+  int e3=3;
+  list.add_front(e1);
+  list.add_front(e2);
+  bool test= list.search(e1) and list.search(e2) and not(list.search(e3));
+  std::cout << std::boolalpha <<  " *** " << test << std::endl;
+}
